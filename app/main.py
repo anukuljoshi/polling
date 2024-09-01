566482de3348f1e6348efee27989da1204ecd152
anukuljoshi <anukuljoshi1@gmail.com>
2024-09-01 22:08:36 +0530

    cookie parameters

--- app/main.py
+++ app/main.py
@@ -1,50 +1,14 @@
 """base file for the application."""
 
-from typing import List, Set, Union
+from typing import Union
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-from pydantic.networks import HttpUrl
+from fastapi import Cookie, FastAPI
+from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    """model for Image"""
-
-    url: HttpUrl
-    name: str
-
-
-class Item(BaseModel):
-    """model for Item"""
-
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: Set[str] = set()
-    image: Union[Image, None] = None
-    images: Union[List[Image], None] = None
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    """example 1 for nested models"""
-    results = {"item_id": item_id, "item": item}
-    return results
-
-
-class Offer(BaseModel):
-    """model for Offer"""
-
-    name: str
-    description: Union[str, None] = None
-    price: float
-    items: List[Item]
-
-
-@app.post("/offers/")
-async def create_offer(offer: Offer):
-    """example 2 for nested models"""
-    return offer
+@app.get("/items/")
+async def read_items(ads_id: Annotated[Union[str, None], Cookie()] = None):
+    """example with Cookie"""
+    return ads_id
