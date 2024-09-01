6ecce5966de20222a30511862de4297800002720
anukuljoshi <anukuljoshi1@gmail.com>
2024-09-01 22:13:25 +0530

    header parameters

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 """base file for the application."""
 
-from typing import Union
+from typing import List, Union
 
-from fastapi import Cookie, FastAPI
+from fastapi import FastAPI, Header
 from typing_extensions import Annotated
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(ads_id: Annotated[Union[str, None], Cookie()] = None):
-    """example with Cookie"""
-    return ads_id
+async def read_items(user_agent: Annotated[Union[str, None], Header()] = None):
+    """example with Header"""
+    return user_agent
+
+
+@app.get("/items/x_tokens/")
+async def read_items2(
+    x_token: Annotated[Union[List[str], None], Header()] = None,
+):
+    """example with multiple Headers"""
+    return {
+        "X-Token Values": x_token
+    }
