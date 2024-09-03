9e19c742fa92334276c0c8d6eb17f035c971dd33
anukuljoshi <anukuljoshi1@gmail.com>
2024-09-04 00:13:05 +0530

    custom exception handling

--- app/main.py
+++ app/main.py
@@ -1,16 +1,63 @@
-from fastapi import FastAPI, File, UploadFile
-from typing_extensions import Annotated
+from fastapi import FastAPI, HTTPException
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
-
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    """example 1 with request files"""
-    return {"file_size": len(file)}
+items = {"foo": "The Foo Wrestlers"}
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    """example 2 with request files"""
-    return {"filename": file.filename}
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    """example with error handling"""
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
+    return {"item": items[item_id]}
+
+
+class UnicornExceptionError(Exception):
+    """custom exception class"""
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+
+@app.exception_handler(UnicornExceptionError)
+async def unicorn_exception_handler(
+    request: Request, exc: UnicornExceptionError
+):
+    """handler function for exception"""
+    return JSONResponse(
+        status_code=418,
+        content={
+            "message": (
+                f"Oops! {exc.name} did something. There goes a rainbow..."
+            )
+        },
+    )
+
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    """read unicorn from path param
+
+    Args:
+    ----
+        name: name of the unicorn
+
+    Raises:
+    ------
+        UnicornExceptionError: custom exception
+
+    Returns:
+    -------
+        details of the unicorn
+    """
+    """example with custom exception"""
+    if name == "yolo":
+        raise UnicornExceptionError(name=name)
+    return {"unicorn_name": name}
