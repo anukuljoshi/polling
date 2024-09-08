5b326309d4b0b82898391975eb02a898f854fb88
anukuljoshi <anukuljoshi1@gmail.com>
2024-09-08 11:19:50 +0530

    dependency injection

--- app/main.py
+++ app/main.py
@@ -1,63 +1,30 @@
-from fastapi import FastAPI, HTTPException
-from starlette.requests import Request
-from starlette.responses import JSONResponse
+from typing import Union
+
+from fastapi import Depends, FastAPI
+from typing_extensions import Annotated
 
 app = FastAPI()
 
-items = {"foo": "The Foo Wrestlers"}
+
+async def common_parameters(
+    q: Union[str, None] = None, skip: int = 0, limit: int = 100
+) -> "dict[str, Union[str, int, None]]":
+    """example of a dependency function"""
+    return {"q": q, "skip": skip, "limit": limit}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str):
-    """example with error handling"""
-    if item_id not in items:
-        raise HTTPException(
-            status_code=404,
-            detail="Item not found",
-            headers={"X-Error": "There goes my error"},
-        )
-    return {"item": items[item_id]}
+CommonDeps = Annotated[
+    "dict[str, Union[str, int, None]]", Depends(common_parameters)
+]
 
 
-class UnicornExceptionError(Exception):
-    """custom exception class"""
-
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-
-@app.exception_handler(UnicornExceptionError)
-async def unicorn_exception_handler(
-    request: Request, exc: UnicornExceptionError
-):
-    """handler function for exception"""
-    return JSONResponse(
-        status_code=418,
-        content={
-            "message": (
-                f"Oops! {exc.name} did something. There goes a rainbow..."
-            )
-        },
-    )
+@app.get("/items/")
+async def read_items(commons: CommonDeps):
+    """example 1 with dependency"""
+    return commons
 
 
-@app.get("/unicorns/{name}")
-async def read_unicorn(name: str):
-    """read unicorn from path param
-
-    Args:
-    ----
-        name: name of the unicorn
-
-    Raises:
-    ------
-        UnicornExceptionError: custom exception
-
-    Returns:
-    -------
-        details of the unicorn
-    """
-    """example with custom exception"""
-    if name == "yolo":
-        raise UnicornExceptionError(name=name)
-    return {"unicorn_name": name}
+@app.get("/users/")
+async def read_users(commons: CommonDeps):
+    """example 2 with dependency"""
+    return commons
