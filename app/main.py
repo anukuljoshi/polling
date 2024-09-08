a8a29f252663a953b399ceb6c8b2a425e37372e9
anukuljoshi <anukuljoshi1@gmail.com>
2024-09-08 17:31:51 +0530

    security - first steps

--- app/main.py
+++ app/main.py
@@ -1,37 +1,13 @@
-from typing import Union
-
 from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from typing_extensions import Annotated
 
 app = FastAPI()
 
-fake_items_db = [
-    {"item_name": "Foo"},
-    {"item_name": "Bar"},
-    {"item_name": "Baz"},
-]
-
-
-class CommonQueryParams:
-    """class for defining common query params"""
-
-    def __init__(
-        self, q: Union[str, None] = None, skip: int = 0, limit: int = 100
-    ) -> None:
-        self.q = q
-        self.skip = skip
-        self.limit = limit
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get("/items/")
-async def read_items(
-    commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)],
-):
-    """example 1 with class as dependency"""
-    response = {}
-    if commons.q:
-        response["q"] = commons.q
-
-    items = fake_items_db[commons.skip : commons.skip + commons.limit]
-    response["items"] = items
-    return items
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    """example with oauth2 token generation"""
+    return {"token": token}
