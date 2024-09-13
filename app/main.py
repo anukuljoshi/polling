03ca17687a226cfa87dfb71ab01a1193a29749e4
anukuljoshi <anukuljoshi1@gmail.com>
2024-09-13 21:09:26 +0530

    middlewares, example with cors

--- app/main.py
+++ app/main.py
@@ -1,119 +1,26 @@
-from typing import Any, Union
-
-from fastapi import (
-    FastAPI,
-    HTTPException,
-    status,
-)
-from fastapi.params import Depends
-from fastapi.security.oauth2 import (
-    OAuth2PasswordBearer,
-    OAuth2PasswordRequestForm,
-)
-from pydantic.main import BaseModel
-from typing_extensions import Annotated
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
-fake_users_db: "dict[Any, Any]" = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
 
-def fake_hash_password(password: str):
-    """function to mock hashing of password"""
-    return "fakehashed" + password
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-class User(BaseModel):
-    """pydantic model class for user"""
-
-    username: str
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
-
-
-class UserInDB(User):
-    """user model with hashed_password"""
-
-    hashed_password: str
-
-
-def get_user(db: Any, username: str):
-    """get user from mock db"""
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def fake_decode_token(token: str):
-    """function to mock token decoding"""
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    """get current user's details"""
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    """get current active user"""
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    """handler for generating token using username and password"""
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(
-            status_code=400, detail="Incorrect username or password"
-        )
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(
-            status_code=400, detail="Incorrect username or password"
-        )
-
-    return {"access_token": user.username, "token_type": "bearer"}
-
-
-@app.get("/users/me")
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-):
-    """handler to get current user data"""
-    return current_user
+@app.get("/")
+async def main():
+    """example with cors middleware"""
+    return {"message": "Hello World"}
