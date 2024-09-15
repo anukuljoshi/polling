85f3d06f64584d1ef60f0779a74cc75dfe63bde7
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2024-09-15 12:45:25 +0530

    static files

--- app/main.py
+++ app/main.py
@@ -1,19 +1,6 @@
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-
-def write_notification(email: str, message: str = ""):
-    """write notification details to log file"""
-    with open("log.txt", "w+", encoding="utf-8") as log_file:
-        content = f"notification for {email}: {message}"
-        log_file.write(content)
-
-
-@app.post("/send-notification/{email}")
-async def send_notification(email: str, background_tasks: BackgroundTasks):
-    """handler to send a notification in background tasks"""
-    background_tasks.add_task(
-        write_notification, email, message="some notification"
-    )
-    return {"message": "Notification sent in the background."}
+app.mount("/static", StaticFiles(directory="static"), name="static")
